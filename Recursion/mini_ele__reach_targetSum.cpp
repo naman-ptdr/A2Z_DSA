dcf5983f05964ceda4bfa7127d65d7a6fa00d1d0
Naman Patidar <namanpatidar5555@gmail.com>
2024-08-02 19:16:16 +0530

    mini_ele__reach_targetSum

---
+++ Recursion/mini_ele__reach_targetSum.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int>& arr, int target){
+    if(target==0) return 0;
+    if(target<0) return INT_MAX;
+
+    int mini = INT_MAX;
+    for(int i=0; i<arr.size(); i++){
+        int ans = solve(arr, target-arr[i]);
+        if(ans!=INT_MAX) mini = min(mini, ans+1);
+    }
+    return mini;
+}
+int main(){
+    vector<int> arr{1, 2, 3};
+    int target = 5;
+    int ans = solve(arr, target);
+    cout<<"Minimum no. of element to reach the target is : "<<ans;
+    return 0;
+}
