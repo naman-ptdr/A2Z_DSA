6534daf73ac146d11f54bd9f3d9d72cbb5406b42
Naman Patidar <namanpatidar5555@gmail.com>
2024-08-02 20:53:25 +0530

    Max_Sum_NonAdjecent

---
+++ Recursion/Max_Sum_NonAdjecent.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(vector<int>& arr, int ind, int sum, int &maxi){
+    if(ind>=arr.size()){
+        maxi = max(sum, maxi);
+        return;
+    }
+
+    solve(arr, ind+2, sum+arr[ind], maxi);
+
+
+    solve(arr, ind+1, sum, maxi);
+
+}
+int main(){
+    vector<int> arr{2, 1, 4, 9};
+    int sum = 0;
+    int maxi = INT_MIN;
+    solve(arr, 0, sum, maxi);
+
+    return 0;
+}
