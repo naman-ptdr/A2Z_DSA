39195e503e6014a2bdf594f8dd0f57141f742c38
Naman Patidar <namanpatidar5555@gmail.com>
2024-09-10 22:03:48 +0530

    G_2 graph implementation using list

--- GRAPH/G_2.cpp
+++ GRAPH/G_2.cpp
@@ -1,15 +1,36 @@
+// using adjacency matrix
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int n, m;
+//     cin>>n>>m;
+//     int adj[n+1][n+1];
+//     for(int i=0; i<m; i++){
+//         int u, v;
+//         cin>>u>>v;
+//         adj[u][v] = 1;
+//         adj[v][u] = 1;
+//     }
+//     return 0;
+// }
+
+// using adjecency  list 
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
     int n, m;
     cin>>n>>m;
-    int adj[n+1][n+1];
+    vector<int> adj[n+1];
+
     for(int i=0; i<m; i++){
         int u, v;
         cin>>u>>v;
-        adj[u][v] = 1;
-        adj[v][u] = 1;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
     return 0;
 }
