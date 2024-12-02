444b6fd0f2ddfa181320c0e6fecdef0d799dd6b4
Naman Patidar <namanpatidar5555@gmail.com>
2024-12-02 17:28:50 +0530

    02_DataTypes

---
+++ 01_LearnBasics/Things To Know In CPP/02_DataTypes.cpp
@@ -0,0 +1,11 @@
+// class Solution {
+//   public:
+//     int dataTypeSize(string str) {
+//         // your code here
+//         if(str == "Integer") return 4;
+//         if(str == "Character") return 1;
+//         if(str == "Long") return 8;
+//         if(str == "Float") return 4;
+//         if(str == "Double") return 8;
+//     }
+// };
