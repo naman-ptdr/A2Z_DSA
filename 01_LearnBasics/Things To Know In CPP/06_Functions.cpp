e449d68c8a0cd7d956e3cacf354e79d9239e406a
Naman Patidar <namanpatidar5555@gmail.com>
2024-12-02 17:45:27 +0530

    06_Functionsunctio

---
+++ 01_LearnBasics/Things To Know In CPP/06_Functions.cpp
@@ -0,0 +1,11 @@
+
+// class Solution {
+//   public:
+//     vector<int> passedBy(int a, int &b) {
+//         // code here
+//         vector<int> ans(2);
+//         ans[0] = a+1;
+//         ans[1] = b+2;
+//         return ans;
+//     }
+// };
