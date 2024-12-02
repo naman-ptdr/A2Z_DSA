328b7d0050542a250b41f0a7f134ec13c0574492
Naman Patidar <namanpatidar5555@gmail.com>
2024-12-02 17:26:35 +0530

    01_userInputOutput

---
+++ 01_LearnBasics/Things To Know In CPP/01_userInputOutput.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Hey, Striver!" << endl;
+    cout << "Hey, Striver!";
+    cin >> x;
+    cout << "Value of x: " << x;
+
+    int y;
+    cin >> x >> y;
+    cout << "Value of x: " << x << " and y: " <<y;
+    return 0;
+}
