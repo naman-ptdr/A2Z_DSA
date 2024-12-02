c690edc3abf0e18406dec5292d30898fdb504bda
Naman Patidar <namanpatidar5555@gmail.com>
2024-12-02 17:29:47 +0530

    03_IfElse

---
+++ 01_LearnBasics/Things To Know In CPP/03_IfElse.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+int age=10;
+
+
+if (age >= 18) {
+    cout << "You are an adult." << endl;
+} else {
+    cout << "You are not an adult." << endl;
+}
+
+}
