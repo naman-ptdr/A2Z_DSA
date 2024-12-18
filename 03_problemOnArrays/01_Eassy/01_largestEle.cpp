95d06b0c00cb20d00cbd6c224bf70b04329d68ca
Naman Patidar <namanpatidar5555@gmail.com>
2024-12-18 20:39:53 +0530

    second largest element in an array

---
+++ 03_problemOnArrays/01_Eassy/01_largestEle.cpp
@@ -0,0 +1,39 @@
+// Solution2: Using a max variable
+// Intuition:
+// We can maintain a max variable that will update whenever the current value is greater than the value in the max variable.  
+
+// Approach: 
+// Create a max variable and initialize it with arr[0].
+// Use a for loop and compare it with other elements of the array
+// If any element is greater than the max value, update max value with the element’s value
+// Print the max variable.
+
+
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+int findLargestElement(int arr[], int n) {
+ 
+  int max = arr[0];
+  for (int i = 0; i < n; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+int main() {
+  int arr1[] = {2,5,1,3,0};
+  int n = 5;
+  int max = findLargestElement(arr1, n);
+  cout << "The largest element in the array is: " << max << endl;
+ 
+  int arr2[] =  {8,10,5,7,9};
+  n = 5;
+  max = findLargestElement(arr2, n);
+  cout << "The largest element in the array is: " << max<<endl;
+  return 0;
+}
+
+
