8fd15c53b9d561eccac33317c364863c288b6c9d
Naman Patidar <namanpatidar5555@gmail.com>
2024-12-19 09:59:55 +0530

    06_MoveZeroToEnd

---
+++ 03_problemOnArrays/01_Eassy/06_MoveZeroToEnd.cpp
@@ -0,0 +1,62 @@
+// Algorithm / Intuition
+// Optimal Approach(Using 2 pointers): 
+// We can optimize the approach using 2 pointers i.e. i and j. The pointer j will point to the first 0 in the array and i will point to the next index.
+
+// Assume, the given array is {1, 0, 2, 3, 2, 0, 0, 4, 5, 1}. Now, initially, we will place the 2-pointers like the following:
+
+
+// The algorithm will be the following.
+
+// Algorithm:
+// First, using a loop, we will place the pointer j. If we don’t find any 0, we will not perform the following steps.
+// After that, we will point i to index j+1 and start moving the pointer using a loop.
+// While moving the pointer i, we will do the following:
+// If a[i] != 0 i.e. a[i] is a non-zero element: We will swap a[i] and a[j]. Now, the current j is pointing to the non-zero element a[i]. So, we will shift the pointer j by 1 so that it can again point to the first zero.
+// Finally, our array will be set in the right manner.
+// Dry run: Please refer video for a detailed dry run.
+
+// The first 2 steps are shown below:
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> moveZeros(int n, vector<int> a) {
+    int j = -1;
+    //place the pointer j:
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 0) {
+            j = i;
+            break;
+        }
+    }
+
+    //no non-zero elements:
+    if (j == -1) return a;
+
+    //Move the pointers i and j
+    //and swap accordingly:
+    for (int i = j + 1; i < n; i++) {
+        if (a[i] != 0) {
+            swap(a[i], a[j]);
+            j++;
+        }
+    }
+    return a;
+}
+
+
+int main()
+{
+    vector<int> arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+    int n = 10;
+    vector<int> ans = moveZeros(n, arr);
+    for (auto &it : ans) {
+        cout << it << " ";
+    }
+    cout << '\n';
+    return 0;
+}
+
