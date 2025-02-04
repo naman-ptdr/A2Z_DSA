501c39e03e1810b6eda8c25941545d41f562f43d
Naman Patidar <namanpatidar5555@gmail.com>
2025-02-05 00:53:35 +0530

    Quick sort

--- 02_SortingAlgo/reviseMerge.cpp
+++ 02_SortingAlgo/reviseMerge.cpp
@@ -31,6 +31,28 @@ void mergeSort(int arr[], int low, int high){
     merge(arr, low, mid, high);
 }
 
+int partition(int arr[], int low, int high){
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while(i<j){
+        while(arr[i] <= pivot && i<= high-1) i++;
+        while(arr[j] > pivot && j>= low+1) j--;
+        if(i<j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+
+void quickSort(int arr[], int low, int high){
+    if(low<high){
+        int pIndex = partition(arr, low, high);
+        quickSort(arr, low, pIndex-1);
+        quickSort(arr, pIndex+1, high);
+    }
+}
+
 int main(){
     int n;
     cin>>n;
