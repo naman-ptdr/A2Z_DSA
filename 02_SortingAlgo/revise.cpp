4405c543276682e1e254f103442357a68c29f60d
Naman Patidar <namanpatidar5555@gmail.com>
2025-02-04 22:40:26 +0530

    bubble sort

--- 02_SortingAlgo/revise.cpp
+++ 02_SortingAlgo/revise.cpp
@@ -10,6 +10,16 @@ void selection_sort(int arr[], int n){
         swap(arr[mini], arr[i]);
     }
 }
+
+void bubble_sort(int arr[], int n){
+    for(int i=n-1; i>=1; i--){
+        for(int j=0; j<i; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
 int main(){
     int n;
     cin >> n;
@@ -17,7 +27,9 @@ int main(){
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
-    selection_sort(arr, n);
+    // selection_sort(arr, n);
+    bubble_sort(arr, n);
     for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+
 return 0;
 }
