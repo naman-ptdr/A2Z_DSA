66df7a72f3b8507dec867d4274e9918ac9b55de7
Naman Patidar <namanpatidar5555@gmail.com>
2025-02-06 18:14:56 +0530

    graph representation with weighted edges by adj list

--- 15_Graphs/1_Learning/02_graphRepresentation.cpp
+++ 15_Graphs/1_Learning/02_graphRepresentation.cpp
@@ -18,17 +18,33 @@ using namespace std;
 
            //adjecency list
 
-int main(){
-    int n, m;
-    cin>>n>>m;
-    vector<int> adj[n+1];
-    for(int i=0; i<m; i++){
-        int u, v;
-        cin>>u >>v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-return 0;
-}
+// int main(){
+//     int n, m;
+//     cin>>n>>m;
+//     vector<int> adj[n+1];
+//     for(int i=0; i<m; i++){
+//         int u, v;
+//         cin>>u >>v;
+//         adj[u].push_back(v);
+//         adj[v].push_back(u);
+//     }
+// return 0;
+// }
 
 // space complexity --- O(2E)   --E --edges
+
+
+
+// adjecency list with edge weight
+int main(){
+    int n, m;
+    cin>>n >>m;
+    vector<pair<int, int>> adj[n+1];
+    for(int i=0; i<m; i++){
+        int u, v, wt;
+        cin>>u >>v >>wt;
+        adj[u].push_back({v, wt});
+        adj[v].push_back({u, wt});
+    }
+    return 0;
+}
