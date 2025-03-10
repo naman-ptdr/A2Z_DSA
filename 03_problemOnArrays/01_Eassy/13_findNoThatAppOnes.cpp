1cbce4986e915efa55855400f898fbb33deedead
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-10 22:55:34 +0530

    find no that appears ones

---
+++ 03_problemOnArrays/01_Eassy/13_findNoThatAppOnes.cpp
@@ -0,0 +1,29 @@
+// 
+// Problem Statement: Given a non-empty array of integers arr, every element appears twice except for one. Find that single one.
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int getSingleElement(vector<int> &arr) {
+    //size of the array:
+    int n = arr.size();
+
+    // XOR all the elements:
+    int xorr = 0;
+    for (int i = 0; i < n; i++) {
+        xorr = xorr ^ arr[i];
+    }
+    return xorr;
+}
+
+int main()
+{
+    vector<int> arr = {4, 1, 2, 1, 2};
+    int ans = getSingleElement(arr);
+    cout << "The single element is: " << ans << endl;
+    return 0;
+}
+
