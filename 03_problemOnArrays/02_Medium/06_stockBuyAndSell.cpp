dad0db9ce14a12c31e73e999f843259c8bc52f93
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-12 19:56:42 +0530

    stock buy and sell

---
+++ 03_problemOnArrays/02_Medium/06_stockBuyAndSell.cpp
@@ -0,0 +1,42 @@
+// Stock Buy And Sell
+
+// Problem Statement: You are given an array of prices where prices[i] is the price of a given stock on an ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock. Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+// Algorithm / Intuition
+// Intuition: We will linearly travel the array. We can maintain a minimum from the start of the array and compare it with every element of the array, if it is greater than the minimum then take the difference and maintain it in max, otherwise update the minimum.
+
+// Approach:
+
+// Create a variable maxPro and store 0 initially.
+// Create a variable minPrice and store some larger value(ex: MAX_VALUE) value initially.
+// Run a for loop from 0 to n.
+// Update the minPrice if it is greater than the current element of the array
+// Take the difference of the minPrice with the current element of the array and compare and maintain it in maxPro.
+// Return the maxPro.
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int> &arr) {
+    int maxPro = 0;
+    int n = arr.size();
+    int minPrice = INT_MAX;
+
+    for (int i = 0; i < arr.size(); i++) {
+        minPrice = min(minPrice, arr[i]);
+        maxPro = max(maxPro, arr[i] - minPrice);
+    }
+    
+    return maxPro;
+}
+
+int main() {
+    vector<int> arr = {7,1,5,3,6,4};
+    int maxPro = maxProfit(arr);
+    cout << "Max profit is: " << maxPro << endl;
+}
+
