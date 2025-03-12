87277ec431c341870786a06b6e4059dce0a64bdb
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-12 19:52:09 +0530

    majority ele occurs more than n/2 times

---
+++ 03_problemOnArrays/02_Medium/03_majorityEle1.cpp
@@ -0,0 +1,62 @@
+// // Find the Majority Element that occurs more than N/2 times
+
+// // Problem Statement: Given an array of N integers, write a program to return an element that occurs more than N/2 times in the given array. You may consider that such an element always exists in the array.
+
+// Optimal Approach: Moore’s Voting Algorithm:
+// Intuition:
+// If the array contains a majority element, its occurrence must be greater than the floor(N/2). Now, we can say that the count of minority elements and majority elements is equal up to a certain point in the array. So when we traverse through the array we try to keep track of the count of elements and the element itself for which we are tracking the count. 
+
+// After traversing the whole array, we will check the element stored in the variable. If the question states that the array must contain a majority element, the stored element will be that one but if the question does not state so, then we need to check if the stored element is the majority element or not. If not, then the array does not contain any majority element.
+
+// Approach: 
+// Initialize 2 variables:
+// Count –  for tracking the count of element
+// Element – for which element we are counting
+// Traverse through the given array.
+// If Count is 0 then store the current element of the array as Element.
+// If the current element and Element are the same increase the Count by 1.
+// If they are different decrease the Count by 1.
+// The integer present in Element should be the result we are expecting 
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int majorityElement(vector<int> v) {
+
+    //size of the given array:
+    int n = v.size();
+    int cnt = 0; // count
+    int el; // Element
+
+    //applying the algorithm:
+    for (int i = 0; i < n; i++) {
+        if (cnt == 0) {
+            cnt = 1;
+            el = v[i];
+        }
+        else if (el == v[i]) cnt++;
+        else cnt--;
+    }
+
+    //checking if the stored element
+    // is the majority element:
+    int cnt1 = 0;
+    for (int i = 0; i < n; i++) {
+        if (v[i] == el) cnt1++;
+    }
+
+    if (cnt1 > (n / 2)) return el;
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
+    int ans = majorityElement(arr);
+    cout << "The majority element is: " << ans << endl;
+    return 0;
+}
+
