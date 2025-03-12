887e4142dfe2fd4981e1eb0fcf5a2a2ceb4f44e2
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-12 19:53:47 +0530

    Maximum sub array sum by kadanes algo

---
+++ 03_problemOnArrays/02_Medium/04_kadanesAlgo_MaxSubarrSum.cpp
@@ -0,0 +1,67 @@
+// Kadane's Algorithm : Maximum Subarray Sum in an Array
+
+// Problem Statement: Given an integer array arr, find the contiguous subarray (containing at least one number) which
+// has the largest sum and returns its sum and prints the subarray.
+
+// Algorithm / Intuition
+// Intuition:
+// The intuition of the algorithm is not to consider the subarray as a part of the answer if its sum is less than 0. A subarray with a sum less than 0 will always reduce our answer and so this type of subarray cannot be a part of the subarray with maximum sum.
+
+// Here, we will iterate the given array with a single loop and while iterating we will add the elements in a sum variable. Now, if at any point the sum becomes less than 0, we will set the sum as 0 as we are not going to consider any subarray with a negative sum. Among all the sums calculated, we will consider the maximum one.
+
+// Thus we can solve this problem with a single loop.
+
+// Approach:
+// The steps are as follows:
+
+// We will run a loop(say i) to iterate the given array.
+// Now, while iterating we will add the elements to the sum variable and consider the maximum one.
+// If at any point the sum becomes negative we will set the sum to 0 as we are not going to consider it as a part of our answer.
+// Note: In some cases, the question might say to consider the sum of the empty subarray while solving this problem. So, in these cases, before returning the answer we will compare the maximum subarray sum calculated with 0(i.e. The sum of an empty subarray is 0). And after that, we will return the maximum one.
+// For e.g. if the given array is {-1, -4, -5}, the answer will be 0 instead of -1 in this case. 
+
+// This is applicable to all the approaches discussed above.
+
+// But if this case is not explicitly mentioned we will not consider this case.
+
+// Note: For a better understanding of intuition, please watch the video at the bottom of the page.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long maxSubarraySum(int arr[], int n) {
+    long long maxi = LONG_MIN; // maximum sum
+    long long sum = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        sum += arr[i];
+
+        if (sum > maxi) {
+            maxi = sum;
+        }
+
+        // If sum < 0: discard the sum calculated
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+
+    // To consider the sum of the empty subarray
+    // uncomment the following check:
+
+    //if (maxi < 0) maxi = 0;
+
+    return maxi;
+}
+
+int main()
+{
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    long long maxSum = maxSubarraySum(arr, n);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+    return 0;
+}
+
