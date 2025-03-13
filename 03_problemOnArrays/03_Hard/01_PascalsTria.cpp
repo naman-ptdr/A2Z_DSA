3b7ddb3e4c287fb20b04cc85d4aad68aa69071b0
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-13 17:19:45 +0530

    Pascals triangle

---
+++ 03_problemOnArrays/03_Hard/01_PascalsTria.cpp
@@ -0,0 +1,61 @@
+// Problem Statement: This problem has 3 variations. They are stated below:
+
+// Variation 1: Given row number r and column number c. Print the element at position (r, c) in Pascal’s triangle.
+
+// Variation 2: Given the row number n. Print the n-th row of Pascal’s triangle.
+
+// Variation 3: Given the number of rows n. Print the first n rows of Pascal’s triangle.
+// Example 1:
+// Input Format:
+//  N = 5, r = 5, c = 3
+// Result:
+//  6 (for variation 1)
+// 1 4 6 4 1 (for variation 2)
+
+// 1 
+// 1 1 
+// 1 2 1 
+// 1 3 3 1 
+// 1 4 6 4 1    (for variation 3)
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return (int)(res);
+}
+
+vector<vector<int>> pascalTriangle(int n) {
+    vector<vector<int>> ans;
+
+    //Store the entire pascal's triangle:
+    for (int row = 1; row <= n; row++) {
+        vector<int> tempLst; // temporary list
+        for (int col = 1; col <= row; col++) {
+            tempLst.push_back(nCr(row - 1, col - 1));
+        }
+        ans.push_back(tempLst);
+    }
+    return ans;
+}
+
+int main()
+{
+    int n = 5;
+    vector<vector<int>> ans = pascalTriangle(n);
+    for (auto it : ans) {
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout << "n";
+    }
+    return 0;
+}
