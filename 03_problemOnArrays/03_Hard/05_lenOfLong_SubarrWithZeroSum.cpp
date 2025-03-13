06fd21af1c77024f0b2234615d28fe301f0dd153
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-13 17:24:56 +0530

    Length of the longest subarray with zero Sum

---
+++ 03_problemOnArrays/03_Hard/05_lenOfLong_SubarrWithZeroSum.cpp
@@ -0,0 +1,32 @@
+// Length of the longest subarray with zero Sum
+
+// Problem Statement: Given an array containing both positive and negative integers, we have to find the length of the longest subarray with the sum of all elements equal to zero.
+
+
+
+
+
+int maxLen(int A[], int n)
+{
+    // Your code here
+    unordered_map<int,int> mpp; 
+    int maxi = 0;
+    int sum = 0; 
+    for(int i = 0;i<n;i++) {
+        sum += A[i]; 
+        if(sum == 0) {
+            maxi = i + 1; 
+        }
+        else {
+            if(mpp.find(sum) != mpp.end()) {
+                maxi = max(maxi, i - mpp[sum]); 
+            }
+            else {
+                mpp[sum] = i;
+            }
+        }   
+    }
+
+    return maxi; 
+}
+
