438691f1a6f37cce6aa93f5e13da3b5286c40941
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-20 12:07:11 +0530

    Search Element in Rotated Sorted Array II

---
+++ 04_BinarySearch/lec1 BS On 1D Arrays/09_SearInRotatedSortArrII.cpp
@@ -0,0 +1,64 @@
+// Search Element in Rotated Sorted Array II
+
+// Problem Statement: Given an integer array arr of size N, sorted in ascending order (may contain duplicate values) and a target value k. Now the array is rotated at some pivot point unknown to you. Return True if k is present and otherwise, return False. 
+
+// Pre-requisite: Search Element in Rotated Sorted Array I & Binary Search algorithm
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchInARotatedSortedArrayII(vector<int>&arr, int k) {
+    int n = arr.size(); // size of the array.
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //if mid points the target
+        if (arr[mid] == k) return true;
+
+        //Edge case:
+        if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+            low = low + 1;
+            high = high - 1;
+            continue;
+        }
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            if (arr[low] <= k && k <= arr[mid]) {
+                //element exists:
+                high = mid - 1;
+            }
+            else {
+                //element does not exist:
+                low = mid + 1;
+            }
+        }
+        else { //if right part is sorted:
+            if (arr[mid] <= k && k <= arr[high]) {
+                //element exists:
+                low = mid + 1;
+            }
+            else {
+                //element does not exist:
+                high = mid - 1;
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    vector<int> arr = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6};
+    int k = 3;
+    bool ans = searchInARotatedSortedArrayII(arr, k);
+    if (!ans)
+        cout << "Target is not present.\n";
+    else
+        cout << "Target is present in the array.\n";
+    return 0;
+}
+
