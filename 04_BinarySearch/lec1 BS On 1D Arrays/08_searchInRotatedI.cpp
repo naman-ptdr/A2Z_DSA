9cb9e9c387011f99b98c83cdf85f42cc8a3c0649
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-20 12:06:02 +0530

    Search Element in a Rotated Sorted Array

---
+++ 04_BinarySearch/lec1 BS On 1D Arrays/08_searchInRotatedI.cpp
@@ -0,0 +1,55 @@
+// Search Element in a Rotated Sorted Array
+
+// Problem Statement: Given an integer array arr of size N, sorted in ascending order (with distinct values) and a target value k. Now the array is rotated at some pivot point unknown to you. Find the index at which k is present and if k is not present return -1.
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int>& arr, int n, int k) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //if mid points the target
+        if (arr[mid] == k) return mid;
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            if (arr[low] <= k && k <= arr[mid]) {
+                //element exists:
+                high = mid - 1;
+            }
+            else {
+                //element does not exist:
+                low = mid + 1;
+            }
+        }
+        else { //if right part is sorted:
+            if (arr[mid] <= k && k <= arr[high]) {
+                //element exists:
+                low = mid + 1;
+            }
+            else {
+                //element does not exist:
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {7, 8, 9, 1, 2, 3, 4, 5, 6};
+    int n = 9, k = 1;
+    int ans = search(arr, n, k);
+    if (ans == -1)
+        cout << "Target is not present.\n";
+    else
+        cout << "The index is: " << ans << "\n";
+    return 0;
+}
+
+
