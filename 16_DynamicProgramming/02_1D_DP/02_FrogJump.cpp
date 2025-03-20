2514afa47f829948b531d97e684ef2ab3012cdf6
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-20 15:59:35 +0530

    Frog jump

---
+++ 16_DynamicProgramming/02_1D_DP/02_FrogJump.cpp
@@ -0,0 +1,31 @@
+// Dynamic Programming : Frog Jump (DP 3)
+
+// Problem Statement:
+
+// Given a number of stairs and a frog, the frog wants to climb from the 0th stair to the (N-1)th stair. At a time the frog can climb either one or two steps. A height[N] array is also given. Whenever the frog jumps from a stair i to stair j, the energy consumed in the jump is abs(height[i]- height[j]), where abs() means the absolute difference. We need to return the minimum energy that can be used by the frog to jump from stair 0 to stair N-1.
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+  vector<int> height{30,10,60,10,60,50};
+  int n=height.size();
+  vector<int> dp(n,-1);
+  dp[0]=0;
+  for(int ind=1;ind<n;ind++){
+      int jumpTwo = INT_MAX;
+        int jumpOne= dp[ind-1] + abs(height[ind]-height[ind-1]);
+        if(ind>1)
+            jumpTwo = dp[ind-2] + abs(height[ind]-height[ind-2]);
+    
+        dp[ind]=min(jumpOne, jumpTwo);
+  }
+  cout<<dp[n-1];
+}
+
+
