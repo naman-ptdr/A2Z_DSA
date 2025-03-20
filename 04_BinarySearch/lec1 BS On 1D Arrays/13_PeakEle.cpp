bf17c253ae1248e01a58613aee2c22e782fc721e
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-20 12:11:42 +0530

    Peak element in Array

---
+++ 04_BinarySearch/lec1 BS On 1D Arrays/13_PeakEle.cpp
@@ -0,0 +1,51 @@
+// Peak element in Array
+
+// Problem Statement: Given an array of length N. Peak element is defined as the element greater than both of its neighbors. Formally, if 'arr[i]' is the peak element, 'arr[i - 1]' < 'arr[i]' and 'arr[i + 1]' < 'arr[i]'. Find the index(0-based) of a peak element in the array. If there are multiple peak numbers, return the index of any peak number.
+
+// Note: For the first element, the previous element should be considered as negative infinity as well as for the last element, the next element should be considered as negative infinity.
+
+// Pre-requisite: Binary Search Algorithm
+
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findPeakElement(vector<int> &arr) {
+    int n = arr.size(); //Size of array.
+
+    // Edge cases:
+    if (n == 1) return 0;
+    if (arr[0] > arr[1]) return 0;
+    if (arr[n - 1] > arr[n - 2]) return n - 1;
+
+    int low = 1, high = n - 2;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //If arr[mid] is the peak:
+        if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
+            return mid;
+
+        // If we are in the left:
+        if (arr[mid] > arr[mid - 1]) low = mid + 1;
+
+        // If we are in the right:
+        // Or, arr[mid] is a common point:
+        else high = mid - 1;
+    }
+    // Dummy return statement
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 5, 1};
+    int ans = findPeakElement(arr);
+    cout << "The peak is at index: " << ans << "\n";
+    return 0;
+}
+
