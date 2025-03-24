34b5c4f89acb44b29827b6bffc53e821cf4b51ae
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-24 16:26:26 +0530

    minimum days to make m bouquts

---
+++ 04_BinarySearch/lec2 BS On Answers/MiniDaysToMakeMBouquets.cpp
@@ -0,0 +1,66 @@
+// Minimum days to make M bouquets
+
+// Problem Statement: You are given 'N’ roses and you are also given an array 'arr'  where 'arr[i]'  denotes that the 'ith' rose will bloom on the 'arr[i]th' day.
+// You can only pick already bloomed roses that are adjacent to make a bouquet. You are also told that you require exactly 'k' adjacent bloomed roses to make a single bouquet.
+// Find the minimum number of days required to make at least ‘m' bouquets each containing 'k' roses. Return -1 if it is not possible.
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool possible(vector<int> &arr, int day, int m, int k) {
+    int n = arr.size(); //size of the array
+    int cnt = 0;
+    int noOfB = 0;
+    // count the number of bouquets:
+    for (int i = 0; i < n; i++) {
+        if (arr[i] <= day) {
+            cnt++;
+        }
+        else {
+            noOfB += (cnt / k);
+            cnt = 0;
+        }
+    }
+    noOfB += (cnt / k);
+    return noOfB >= m;
+}
+int roseGarden(vector<int> arr, int k, int m) {
+    long long val = m * 1ll * k * 1ll;
+    int n = arr.size(); //size of the array
+    if (val > n) return -1; //impossible case.
+    //find maximum and minimum:
+    int mini = INT_MAX, maxi = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        mini = min(mini, arr[i]);
+        maxi = max(maxi, arr[i]);
+    }
+
+    //apply binary search:
+    int low = mini, high = maxi;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (possible(arr, mid, m, k)) {
+            high = mid - 1;
+        }
+        else low = mid + 1;
+    }
+    return low;
+}
+
+int main()
+{
+    vector<int> arr = {7, 7, 7, 7, 13, 11, 12, 7};
+    int k = 3;
+    int m = 2;
+    int ans = roseGarden(arr, k, m);
+    if (ans == -1)
+        cout << "We cannot make m bouquets.\n";
+    else
+        cout << "We can make bouquets on day " << ans << "\n";
+    return 0;
+}
+
