b3492580d5e719d3679d7554e5be9588d71dd67e
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-25 19:13:13 +0530

    split array

---
+++ 04_BinarySearch/lec2 BS On Answers/10_SplitArray.cpp
@@ -0,0 +1,57 @@
+// Split Array - Largest Sum
+
+// Problem Statement: Given an integer array ‘A’ of size ‘N’ and an integer ‘K'. Split the array ‘A’ into ‘K’ non-empty subarrays such that the largest sum of any subarray is minimized. Your task is to return the minimized largest sum of the split.
+// A subarray is a contiguous part of the array.
+
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int countPartitions(vector<int> &a, int maxSum) {
+    int n = a.size(); //size of array.
+    int partitions = 1;
+    long long subarraySum = 0;
+    for (int i = 0; i < n; i++) {
+        if (subarraySum + a[i] <= maxSum) {
+            //insert element to current subarray
+            subarraySum += a[i];
+        }
+        else {
+            //insert element to next subarray
+            partitions++;
+            subarraySum = a[i];
+        }
+    }
+    return partitions;
+}
+
+int largestSubarraySumMinimized(vector<int> &a, int k) {
+    int low = *max_element(a.begin(), a.end());
+    int high = accumulate(a.begin(), a.end(), 0);
+    //Apply binary search:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int partitions = countPartitions(a, mid);
+        if (partitions > k) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
+
+int main()
+{
+    vector<int> a = {10, 20, 30, 40};
+    int k = 2;
+    int ans = largestSubarraySumMinimized(a, k);
+    cout << "The answer is: " << ans << "\n";
+    return 0;
+}
+
