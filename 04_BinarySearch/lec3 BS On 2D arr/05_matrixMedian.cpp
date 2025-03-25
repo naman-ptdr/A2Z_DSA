67ed1dfbe4b669dadce29d938b5be0cd1b45c3f7
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-25 21:48:47 +0530

    Matrix median

---
+++ 04_BinarySearch/lec3 BS On 2D arr/05_matrixMedian.cpp
@@ -0,0 +1,71 @@
+// Median of Row Wise Sorted Matrix
+
+// Problem Statement: Given a row-wise sorted matrix of size MXN, where M is no. of rows and N is no. of columns, find the median in the given matrix.
+
+// Note: MXN is odd.
+
+
+
+                                    
+#include<bits/stdc++.h>
+using namespace std;
+
+int upperBound(vector<int> &arr, int x, int n) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] > x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+int countSmallEqual(vector<vector<int>> &matrix, int m, int n, int x) {
+    int cnt = 0;
+    for (int i = 0; i < m; i++) {
+        cnt += upperBound(matrix[i], x, n);
+    }
+    return cnt;
+}
+
+int median(vector<vector<int>> &matrix, int m, int n) {
+    int low = INT_MAX, high = INT_MIN;
+
+    //point low and high to right elements:
+    for (int i = 0; i < m; i++) {
+        low = min(low, matrix[i][0]);
+        high = max(high, matrix[i][n - 1]);
+    }
+
+    int req = (n * m) / 2;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int smallEqual = countSmallEqual(matrix, m, n, mid);
+        if (smallEqual <= req) low = mid + 1;
+        else high = mid - 1;
+    }
+    return low;
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 2, 3, 4, 5},
+        {8, 9, 11, 12, 13},
+        {21, 23, 25, 27, 29}
+    };
+    int m = matrix.size(), n = matrix[0].size();
+    int ans = median(matrix, m, n);
+    cout << "The median element is: " << ans << endl;
+    return 0;
+}
+                                    
+                                
