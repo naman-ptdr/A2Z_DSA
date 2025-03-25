52e53e2375318c892ced18d521b9b1f7229c3989
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-25 21:46:51 +0530

    Search in a row and column-wise sorted matrix

---
+++ 04_BinarySearch/lec3 BS On 2D arr/03_searchInRowColWiseSorted.cpp
@@ -0,0 +1,40 @@
+// Search in a row and column-wise sorted matrix
+
+// Problem Statement: You have been given a 2-D array 'mat' of size 'N x M' where 'N' and 'M' denote the number of rows and columns, respectively. The elements of each row and each column are sorted in non-decreasing order.
+// But, the first element of a row is not necessarily greater than the last element of the previous row (if it exists).
+// You are given an integer ‘target’, and your task is to find if it exists in the given 'mat' or not.
+
+// Pre-requisite: Search in a 2D sorted matrix
+
+
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchElement(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int row = 0, col = m - 1;
+
+    //traverse the matrix from (0, m-1):
+    while (row < n && col >= 0) {
+        if (matrix[row][col] == target) return true;
+        else if (matrix[row][col] < target) row++;
+        else col--;
+    }
+    return false;
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19},
+        {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24},
+        {18, 21, 23, 26, 30}
+    };
+    searchElement(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+}
+
