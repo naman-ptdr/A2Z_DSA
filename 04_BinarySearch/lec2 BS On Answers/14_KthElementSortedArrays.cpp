78f862ae392883a880f0ad5fbc97c6b41d146b23
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-25 19:17:15 +0530

    K-th Element of two sorted arrays

---
+++ 04_BinarySearch/lec2 BS On Answers/14_KthElementSortedArrays.cpp
@@ -0,0 +1,52 @@
+// K-th Element of two sorted arrays
+
+// Pre-requisite: Median of 2 sorted arrays
+
+// Problem Statement: Given two sorted arrays of size m and n respectively, you are tasked with finding the element that would be at the kth position of the final sorted array.
+
+// Disclaimer: Don’t jump directly to the solution, try it out yourself first. 
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int kthElement(vector<int> &a, vector<int>& b, int m, int n, int k) {
+    if (m > n) return kthElement(b, a, n, m, k);
+
+    int left = k; //length of left half
+
+    //apply binary search:
+    int low = max(0, k - n), high = min(k, m);
+    while (low <= high) {
+        int mid1 = (low + high) >> 1;
+        int mid2 = left - mid1;
+        //calculate l1, l2, r1 and r2;
+        int l1 = INT_MIN, l2 = INT_MIN;
+        int r1 = INT_MAX, r2 = INT_MAX;
+        if (mid1 < m) r1 = a[mid1];
+        if (mid2 < n) r2 = b[mid2];
+        if (mid1 - 1 >= 0) l1 = a[mid1 - 1];
+        if (mid2 - 1 >= 0) l2 = b[mid2 - 1];
+
+        if (l1 <= r2 && l2 <= r1) {
+            return max(l1, l2);
+        }
+
+        //eliminate the halves:
+        else if (l1 > r2) high = mid1 - 1;
+        else low = mid1 + 1;
+    }
+    return 0; //dummy statement
+
+}
+
+int main()
+{
+    vector<int> a = {2, 3, 6, 7, 9};
+    vector<int> b = {1, 4, 8, 10};
+    cout << "The k-th element of two sorted array is: " <<
+            kthElement(a, b, a.size(), b.size(), 5) << '\n';
+}
+        
+        
