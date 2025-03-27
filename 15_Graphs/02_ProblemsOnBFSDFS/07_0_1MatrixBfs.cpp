eaa665bc7e0c4454130845315ddfbd9373aa2d5d
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-27 22:58:55 +0530

    Distance of Nearest Cell having 1

---
+++ 15_Graphs/02_ProblemsOnBFSDFS/07_0_1MatrixBfs.cpp
@@ -0,0 +1,85 @@
+// Distance of Nearest Cell having 1
+
+// Problem Statement: Given a binary grid of N*M. Find the distance of the nearest 1 in the grid for each cell.
+
+// The distance is calculated as |i1  - i2| + |j1 - j2|, where i1, j1 are the row number and column number of the current cell, and i2, j2 are the row number and column number of the nearest cell having value 1.
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution 
+{
+    public:
+    //Function to find the distance of nearest 1 in the grid for each cell.
+	vector<vector<int>>nearest(vector<vector<int>>grid)
+	{
+	    int n = grid.size(); 
+	    int m = grid[0].size(); 
+	    // visited and distance matrix
+	    vector<vector<int>> vis(n, vector<int>(m, 0)); 
+	    vector<vector<int>> dist(n, vector<int>(m, 0)); 
+	    // <coordinates, steps>
+	    queue<pair<pair<int,int>, int>> q; 
+	    // traverse the matrix
+	    for(int i = 0;i<n;i++) {
+	        for(int j = 0;j<m;j++) {
+	            // start BFS if cell contains 1
+	            if(grid[i][j] == 1) {
+	                q.push({{i,j}, 0}); 
+	                vis[i][j] = 1; 
+	            }
+	            else {
+	                // mark unvisited 
+	                vis[i][j] = 0; 
+	            }
+	        }
+	    }
+	    
+	    int delrow[] = {-1, 0, +1, 0}; 
+	    int delcol[] = {0, +1, 0, -1}; 
+	    
+	    // traverse till queue becomes empty
+	    while(!q.empty()) {
+	        int row = q.front().first.first; 
+	        int col = q.front().first.second; 
+	        int steps = q.front().second; 
+	        q.pop(); 
+	        dist[row][col] = steps; 
+	        // for all 4 neighbours
+	        for(int i = 0;i<4;i++) {
+	            int nrow = row + delrow[i]; 
+	            int ncol = col + delcol[i]; 
+	            // check for valid unvisited cell
+	            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m 
+	            && vis[nrow][ncol] == 0) {
+	                vis[nrow][ncol] = 1; 
+	                q.push({{nrow, ncol}, steps+1});  
+	            }
+	        }
+	    }
+	    // return distance matrix
+	    return dist; 
+	}
+};
+
+int main(){
+    vector<vector<int>>grid{
+        {0,1,1,0},
+        {1,1,0,0},
+        {0,0,1,1}
+    };
+	
+	Solution obj;
+	vector<vector<int>> ans = obj.nearest(grid);
+		
+	for(auto i: ans){
+		for(auto j: i)
+			cout << j << " ";
+		cout << "\n";
+	}
+	
+	return 0;
+}
