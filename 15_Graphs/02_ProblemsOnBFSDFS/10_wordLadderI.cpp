bc62ed4ff63d2fe169699e913e31ddf8bd6cf3e5
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-28 22:59:37 +0530

    Word Ladder - I : G-29

---
+++ 15_Graphs/02_ProblemsOnBFSDFS/10_wordLadderI.cpp
@@ -0,0 +1,92 @@
+// Word Ladder - I : G-29
+
+// Given are the two distinct words startWord and targetWord, and a list denoting wordList of unique words of equal lengths. Find the length of the shortest transformation sequence from startWord to targetWord.
+
+// In this problem statement, we need to keep the following conditions in mind:
+
+// A word can only consist of lowercase characters.
+// Only one letter can be changed in each transformation.
+// Each transformed word must exist in the wordList including the targetWord.
+// startWord may or may not be part of the wordList
+// Note:  If there’s no possible way to transform the sequence from startWord to targetWord return 0.
+
+// Example 1:
+
+// Input:
+// wordList = {"des","der","dfr","dgt","dfs"}
+// startWord = "der", targetWord = "dfs"
+// Output:
+// 3
+// Explanation: 
+// The length of the smallest transformation sequence from "der" to 
+// "dfs" is 3 i.e. "der" -> (replace ‘e’ by ‘f’) -> "dfr" -> (replace ‘r’ by ‘s’)  -> "dfs". So, it takes 3 different strings for us to reach the targetWord. Each of these strings are present in the wordList.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+class Solution
+{
+public:
+    int wordLadderLength(string startWord, string targetWord,
+                         vector<string> &wordList)
+    {
+    // Creating a queue ds of type {word,transitions to reach ‘word’}.
+        queue<pair<string, int>> q;
+
+        // BFS traversal with pushing values in queue 
+        // when after a transformation, a word is found in wordList.
+        q.push({startWord, 1});
+
+        // Push all values of wordList into a set
+        // to make deletion from it easier and in less time complexity.
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        st.erase(startWord);
+        while (!q.empty())
+        {
+            string word = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+
+            // we return the steps as soon as
+            // the first occurence of targetWord is found.
+            if (word == targetWord)
+                return steps;
+
+            for (int i = 0; i < word.size(); i++)
+            {
+                // Now, replace each character of ‘word’ with char
+                // from a-z then check if ‘word’ exists in wordList.
+                char original = word[i];
+                for (char ch = 'a'; ch <= 'z'; ch++)
+                {
+                    word[i] = ch;
+                    // check if it exists in the set and push it in the queue.
+                    if (st.find(word) != st.end())
+                    {
+                        st.erase(word);
+                        q.push({word, steps + 1});
+                    }
+                }
+                word[i] = original;
+            }
+        }
+        // If there is no transformation sequence possible
+        return 0;
+    }
+};
+ 
+int main()
+{
+ 
+    vector<string> wordList = {"des", "der", "dfr", "dgt", "dfs"};
+    string startWord = "der", targetWord = "dfs";
+ 
+    Solution obj;
+ 
+    int ans = obj.wordLadderLength(startWord, targetWord, wordList);
+ 
+    cout << ans;
+    cout << endl;
+    return 0;
+}
