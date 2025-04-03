47cbd0114889b23a78a048a1dc6339fd4f85ce44
Naman_ptdr <namanpatidar5555@gmail.com>
2025-04-03 16:21:20 +0530

    linked list introduction

--- 06_LinkedList/01_Learn1D LinkedList/Introduction.cpp
+++ 06_LinkedList/01_Learn1D LinkedList/Introduction.cpp
@@ -59,9 +59,56 @@ bool searchInLL(Node* head, int val){
     return false;
 }
 
+Node* removeHead(Node* head){
+    if(head==NULL) return head;
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+}
+
+Node* removeTail(Node* head){
+    if(head==NULL || head->next==NULL) return NULL;
+    Node* temp = head;
+    while(temp->next->next){
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = NULL;
+    return head;
+}
+
+Node* removeK(Node* head, int k){
+    if(head==NULL) return head;
+    if(k==1){
+        Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    int cnt = 0;
+    Node* prev = NULL;
+    Node* temp = head;
+    while(temp){
+        cnt++;
+        if(cnt == k){
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+Node* insertHead(Node* head, int val){
+    Node* temp = new Node(val, head);
+    return temp;
+}
 
 int main(){
-    vector<int> arr = {8, 3, 9, 22, 88};
+    vector<int> arr = {8, 3, 9, 22, 7, 2, 50};
     Node* head = convertToLL(arr);
     cout<<"Head of LL : "<<head->data<<endl;
     cout<<"Linked List : ";
@@ -69,5 +116,23 @@ int main(){
     int length = findLength(head);
     cout<<"Length Of LL "<<length<<endl;
 
-    cout<<"Is 9 is in LL or not : "<< searchInLL(head, 9);
+    cout<<"Is 9 is in LL or not : "<< searchInLL(head, 9)<<endl;
+
+    head = removeHead(head);
+    cout<<"Head of new LL : "<<head->data<<endl;
+
+    head = removeTail(head);
+    cout<<"New Linked List after remove tail : ";
+    printLL(head);
+
+    head = removeK(head, 3);
+    cout<<"New Linked List after remove 3rd element : ";
+    printLL(head);
+
+    // Insert at head
+
+    // head = new Node(5, head);
+
+    head = insertHead(head, 5);
+    cout<<head->data<<endl;
 }
